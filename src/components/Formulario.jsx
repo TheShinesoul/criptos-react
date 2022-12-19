92658a0ee4c84fc12c4b99dffd583162b2d8f1c0
TheShinesoul <jimcont.hector@gmail.com>
2022-12-19 17:44:00 -0600

    feat: :sparkles: se agrega el formulario al componente principal
    se agrega el formulario los hooks y se etablece para la visualizacion de ellos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from '@emotion/styled'
+import useSelectMonedas from '../hooks/useSelectMonedas'
+import Error from './Error'
+import { monedas } from '../data/monedas'
 
-const Formulario = () => {
-    const InputSubmit = styled.input`
+
+const InputSubmit = styled.input`
         background-color: #9497FF;
         border: none;
         width: 100%;
@@ -14,18 +17,76 @@ const Formulario = () => {
         border-radius: 5px;
         transition: background-color .3s ease;
         cursor: pointer;
-        
+        margin-top: 30px;
+
         &:hover{
             background-color: #7a7dfe;
         }
     `
+
+const Formulario = () => {
+    const [criptos, setCriptos] = useState([])
+    const [error, setError] = useState(false)
+
+
+    const [moneda, SelectMonedas] = useSelectMonedas('Elige tu moneda', monedas)
+    const [criptoMoneda, SelectCripto] = useSelectMonedas('Elige tu criptomoneda', criptos)
+
+    useEffect(() => {
+        const consultarApi = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
+            const respuesta = await fetch(url)
+            const resultado = await respuesta.json()
+
+            const arrayCripto = resultado.Data.map(cripto => {
+                const objeto = {
+                    id: cripto.CoinInfo.Name,
+                    nombre: cripto.CoinInfo.FullName
+                }
+
+                return (objeto)
+            })
+
+            setCriptos(arrayCripto)
+        }
+
+        consultarApi()
+    }, [])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if ([moneda, criptoMoneda].includes('')) {
+            console.log('error');
+            setError(true)
+            return
+        }
+
+        setError(false)
+
+        
+
+    }
+
+    // SelectMonedas()
+
     return (
-        <form>
-            <InputSubmit 
-                type='submit' 
-                value='Cotizar' 
-            />
-        </form>
+        <>
+            
+            {error && <Error>Todos los campos son obligatorios</Error>}
+
+            <form onSubmit={handleSubmit}>
+
+                <SelectMonedas />
+                <SelectCripto />
+
+                <InputSubmit
+                    type='submit'
+                    value='Cotizar'
+                />
+            </form>
+
+        </>
     )
 }
 
