807bf8ad088734d5a0c4896e1e3f6642531d2876
TheShinesoul <jimcont.hector@gmail.com>
2022-12-19 03:27:46 -0600

    feat: :sparkles: add styled components
    create 3 styled componets for examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
 import './index.css'
+import styled from "@emotion/styled";
+import ImagenCripto from './assets/img/imagen-criptos.png'
 
+const Heading = styled.h1`
+    font-family: 'Lato', sans-serif;
+    color:#fff;
+`
 
+const Imagen = styled.img`
+    max-width:400px;
+    width:80%;
+    margin: 100px auto 0 auto;
+    display:block;`
+
+const Contenedor = styled.div`
+    max-width:900px;
+    margin:0 auto;
+    width:90%;
+    @media (min-width: 992px) {
+        display: grid;
+        grid-template-columns: repeat(2,1fr);
+        column-gap: 2rem;
+    }`
 
 function App() {
 
-    cons
+
 
     return (
-        <>
-            <h1>Hello word</h1>
-        </>
+        <Contenedor>
+            <Imagen
+                src={ImagenCripto}
+                alt='Imagen Criptomonedas'
+            />
+            <Heading 
+            >Hello word</Heading>
+        </Contenedor>
     )
 }
 
