8152dd89758b59fac53447a40e39f313fd905660
TheShinesoul <jimcont.hector@gmail.com>
2022-12-27 19:26:28 -0600

    feat: :sparkles: add state for save api result
    state para guardar la informacion del formulario
    
    se guarda el valor de los input y se pasa a un state que vive en app.js

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -24,7 +24,7 @@ const InputSubmit = styled.input`
         }
     `
 
-const Formulario = () => {
+const Formulario = ({setMonedas}) => {
     const [criptos, setCriptos] = useState([])
     const [error, setError] = useState(false)
 
@@ -63,6 +63,10 @@ const Formulario = () => {
         }
 
         setError(false)
+        setMonedas({
+            moneda,
+            criptoMoneda
+        })
 
         
 
